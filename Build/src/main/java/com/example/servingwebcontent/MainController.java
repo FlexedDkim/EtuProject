014989485c50d2389cbb665530e0bf93e5af9e58
Dmitry <me@biarby.ru>
2023-11-25 22:11:52 +0300

    Revert "1"
    This reverts commit c8c47c19536222e20f5bdc3441ca269de7394ea2.

---
+++ Build/src/main/java/com/example/servingwebcontent/MainController.java
@@ -0,0 +1,46 @@
+package com.example.servingwebcontent;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main(HttpSession session,Model model) {
+        if (session.getAttribute("user") == null) {
+            return "redirect:/login";
+        }
+        else
+        {
+            return "redirect:/dashboard";
+        }
+    }
+    @GetMapping("/login")
+    public String login(HttpSession session,Model model) {
+        if (session.getAttribute("user") == null) {
+            return "login";
+        }
+        else
+        {
+            return "redirect:/dashboard";
+        }
+    }
+    @GetMapping("/register")
+    public String register(HttpSession session,Model model) {
+        if (session.getAttribute("user") == null) {
+            return "register";
+        }
+        else
+        {
+            return "redirect:/dashboard";
+        }
+    }
+    @GetMapping("/error")
+    public String error(Model model) {
+        return "error";
+    }
+}
